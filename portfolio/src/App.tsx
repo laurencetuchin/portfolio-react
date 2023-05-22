5167d9183183fce9a242da76de140e9ad8131b56
laurencetuchin <laurencetuchin@gmail.com>
2023-05-22 14:18:04 +1000

    Initial commit

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+	return (
+		<>
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>My name is Laurence Tuchin</h1>
+		</>
+	);
+}
+
+export default App;
