78fb913ce663dc5fe1b488661fbed33977d72bc8
laurencetuchin <laurencetuchin@gmail.com>
2023-05-25 14:53:51 +1000

    Add Error page

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,19 +1,12 @@
+import "tailwindcss/tailwind.css";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Intro from "./Intro";
 
 function App() {
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>My name is Laurence Tuchin</h1>
+			<div></div>
+			<Intro />
 		</>
 	);
 }
--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "tailwindcss/tailwind.css";
 import App from "./App.tsx";
+import Error from "./Error.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <div>Hello world!</div>,
+		element: <div>Home</div>,
+		errorElement: <Error />,
+	},
+	{
+		path: "/projects",
+		element: <div>Projects</div>,
+		errorElement: <Error />,
 	},
 ]);
 
