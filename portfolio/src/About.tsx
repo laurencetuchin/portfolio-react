0e26cd2fa6b6c9bd4d7d4559167418e43524b996
laurencetuchin <laurencetuchin@gmail.com>
2023-05-26 21:57:55 +1000

    Update about

--- portfolio/src/About.tsx
+++ portfolio/src/About.tsx
@@ -1,31 +1,33 @@
 export default function About() {
 	return (
-		<div className="sm:py-8 md:px-8 md:py-12 lg:py-24">
-			<h2 className="mt-3 text-left text-lg font-bold uppercase text-slate-600 sm:text-xl md:mb-8">
-				About
-			</h2>
-			<div className="text-left text-lg tracking-tight text-slate-600">
-				<p className="py-6 sm:py-4">
-					Hello, my name is Laurence. I am a Junior Software Developer who's
-					very passionate about creating beautiful seemless experiences for
-					users and delivering the best product you can dream. I enjoy thinking
-					about lots of possibilities so that the software delivered exceeds
-					expectations.
-				</p>
-				<p className="py-6 sm:py-4">
-					My focus is on creating solutions for clients that exceed their
-					expectations. I love getting things done and building products that
-					help people and communities. Continuous learning is incredibly
-					important to me and one of the main reasons I entered into this field.
-					I love building projects in my spare time to help me learn new things
-					and to continue becoming a better developer.
-				</p>
-				<p className="py-6 sm:py-4">
-					When I'm not behind the computer trying to find the best way of doing
-					something, you will find me... hiking outdoors, playing soccer with
-					friends, snowboarding and immersing myself in movies!
-				</p>
+		<section id="about">
+			<div className="sm:py-8 md:px-8 md:py-12 lg:py-24">
+				<h2 className="mt-3 text-left text-lg font-bold uppercase text-slate-600 sm:text-xl md:mb-8">
+					About
+				</h2>
+				<div className="text-left text-lg tracking-tight text-slate-600">
+					<p className="py-6 sm:py-4">
+						Hello, my name is Laurence. I am a Junior Software Developer who's
+						very passionate about creating beautiful seemless experiences for
+						users and delivering the best product you can dream. I enjoy
+						thinking about lots of possibilities so that the software delivered
+						exceeds expectations.
+					</p>
+					<p className="py-6 sm:py-4">
+						My focus is on creating solutions for clients that exceed their
+						expectations. I love getting things done and building products that
+						help people and communities. Continuous learning is incredibly
+						important to me and one of the main reasons I entered into this
+						field. I love building projects in my spare time to help me learn
+						new things and to continue becoming a better developer.
+					</p>
+					<p className="py-6 sm:py-4">
+						When I'm not behind the computer trying to find the best way of
+						doing something, you will find me... hiking outdoors, playing soccer
+						with friends, snowboarding and immersing myself in movies!
+					</p>
+				</div>
 			</div>
-		</div>
+		</section>
 	);
 }
