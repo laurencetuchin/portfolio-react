2fe54935b9d0dba8bc6f0d9914737176de34d7d8
laurencetuchin <laurencetuchin@gmail.com>
2023-05-27 21:07:40 +1000

    Update tech stack

--- portfolio/src/TechStack.tsx
+++ portfolio/src/TechStack.tsx
@@ -4,6 +4,26 @@ export default function TechStack() {
 			<h2 className="mt-3 text-lg font-medium tracking-tight text-slate-600 sm:text-xl md:mb-8">
 				Tech Stack
 			</h2>
+			<div>
+				<ul>
+					<div>
+						<span>
+							<li>React</li>
+							<li>JavaScript</li>
+							<li>TypeScript</li>
+							<li>Java</li>
+							<li>Spring</li>
+						</span>
+						<span>
+							<li>Postman</li>
+							<li>HTML</li>
+							<li>CSS</li>
+							<li>Git</li>
+							<li>TailwindCSS</li>
+						</span>
+					</div>
+				</ul>
+			</div>
 		</div>
 	);
 }
